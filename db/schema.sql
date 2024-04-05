8ac8f7918cca4f06a56d08d72d4b2af7d0cd2fef
Ryan945 <sarathryan@yahoo.com>
2024-04-04 18:13:02 -0700

    created readme

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DATABASE IF EXISTS tech_blog_db;
+
+CREATE DATABASE tech_blog_db;
---
+++ models/index.js
@@ -0,0 +1,35 @@
+const User = require('./models/User');
+const Post = require('./models/Post');
+const Comment = require('./models/comment');
+
+
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+})
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+})
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+})
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+})
+
+
+module.exports = {
+    User,
+    Post,
+    Comment
+};
